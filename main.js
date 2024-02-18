b18e0af50dacaa8c4da90b820527b6dde09ec0c5
MrinellChandraRay <mrinellroy@gmail.com>
2024-02-18 17:46:54 +0600

    five

---
+++ main.js
@@ -0,0 +1,36 @@
+let num=0;
+let perSit=550;
+const btns=document.querySelectorAll('.btn');
+const counts=document.getElementById('count');
+const tk=document.getElementById('tk');
+const sits=document.getElementById('sit');
+//const ele=document.createElement('p');
+//const eleT=document.createTextNode('Mrinell');
+//ele.appendChild(eleT);
+// const sit=document.getElementById('seates').appendChild(ele);
+
+btns.forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+        //const sit=document.getElementById('seates').appendChild(ele);
+        //sit.style.maxHeight='100vh';
+        const styless=e.currentTarget.innerHTML;
+        //console.log(styless);
+        const style = e.currentTarget.classList.add('bg');
+        const styles=e.currentTarget.classList;
+        //console.log(style);
+        if(styles.contains('btn-active')){
+            num++;
+            perSit;
+        }
+        counts.innerHTML=num;
+        tk.innerHTML=perSit;
+        sits.innerHTML=styless;
+
+        
+
+    })
+})
+
+function myF(){
+    
+}
