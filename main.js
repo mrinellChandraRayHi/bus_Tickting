58b5d4e63940e40be5107066c6c793741f09a03d
MrinellChandraRay <mrinellroy@gmail.com>
2024-02-18 19:31:32 +0600

    add to my full web site

--- main.js
+++ main.js
@@ -4,20 +4,11 @@ const btns=document.querySelectorAll('.btn');
 const counts=document.getElementById('count');
 const tk=document.getElementById('tk');
 const sits=document.getElementById('sit');
-//const ele=document.createElement('p');
-//const eleT=document.createTextNode('Mrinell');
-//ele.appendChild(eleT);
-// const sit=document.getElementById('seates').appendChild(ele);
-
 btns.forEach((btn)=>{
     btn.addEventListener('click',(e)=>{
-        //const sit=document.getElementById('seates').appendChild(ele);
-        //sit.style.maxHeight='100vh';
         const styless=e.currentTarget.innerHTML;
-        //console.log(styless);
         const style = e.currentTarget.classList.add('bg');
         const styles=e.currentTarget.classList;
-        //console.log(style);
         if(styles.contains('btn-active')){
             num++;
             perSit;
